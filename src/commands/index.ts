9cdac612fced4222596cc3589a98ab671c1fafea
Barba <vinicius.gomes@ramper.com.br>
2022-04-28 20:18:56 -0300

    feat(factories): implements the command to create factories

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,8 @@
 import { Command } from 'commander';
+import generateDomainFactory from './generateDomainFactory';
 import generateModule from './generateModule';
 
 export default function init(args: string[], program: Command): void {
   generateModule(args, program);
+  generateDomainFactory(args, program);
 }
